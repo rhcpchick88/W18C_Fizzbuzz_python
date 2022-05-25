a205928a667fe9cea74e8cd4c4617e5afdec548d
samantha <langridge.se@gmail.com>
2022-05-24 20:33:43 -0600

    created python script for fizzbuzz

---
+++ app.py
@@ -0,0 +1,22 @@
+
+
+x = 150
+
+if x % 3 == 0 and x % 5 == 0 :
+    print(str(x) + " :FizzBuzz")    
+elif x % 3 == 0: 
+    print(str(x) + " :Fizz")
+elif x % 5 == 0: 
+    print(str(x) + " :Buzz")
+
+
+num_list = [11,88,56,42,30,3,7,78,96,93,5,66,99,35,15]
+
+for num in num_list:
+    if num % 3 == 0 and num % 5 == 0 :
+        print(str(num) + " :FizzBuzz")    
+    elif num % 3 == 0: 
+        print(str(num) + " :Fizz")
+    elif num % 5 == 0: 
+        print(str(num) + " :Buzz")
+
